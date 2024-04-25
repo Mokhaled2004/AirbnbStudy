249a1b0c7c2c2d160196d0539b428bde17023abc
Mokhaled2004 <mokhaled2004@gmail.com>
2024-04-25 01:55:10 -0700

    Solo Leveling

--- console.py
+++ console.py
@@ -31,13 +31,67 @@ class HBNBCommand(cmd.CMD):
     Attributes:
         prompt (str): The command prompt.
     """
-
     prompt = "(hbnb)"
+
+
+    __classes = {
+
+        "BaseModel",
+        "User",
+        "State",
+        "City",
+        "Place",
+        "Amenity",
+        "Review"
+      }
+
+
+
 
     def emptyline(self):
         """Do nothing """
 
         pass
+
+
+    def default(self,arg):
+
+        "Default behavior for cmd module when input is invalid"
+
+        argdict = {
+            
+            "all": self.do_all,
+            "show": self.do_show,
+            "destroy": self.do_destroy,
+            "count": self.do_count,
+            "update": self.do_update
+
+           }
+
+
+        match = re.search(r"\.", arg)
+        if march is not None:
+            argl = [arg[:match.span()[0]], arg[match.span()[1]:]]
+            match = re.search(r"\((.*?)\)", argl)
+            if match is not None:
+                command = [argl[1][:match.span()[0]], match.group()[1:-1]]
+                if command[0] in argdict.keys():
+                    call = "{} {}".format(argl[0] , command[1])
+                    return argdict[command[0]](call)
+        print("*** Unknown syntax: {}".format(arg))
+        return False
+
+
+
+
+
+
+
+
+
+
+
+
 
     def do_quit(self, arg):
         """Quit the program """
